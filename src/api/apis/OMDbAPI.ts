Lorite/obsidian-media-db-plugin

commit 62347f6a38c578dd9633ed7d3389173d0ffa2d98

Replace game for videogame, modify tags
Refactor media type terminology from 'Game' to 'Videogame' across models, APIs, and settings for consistency and clarity. Also, replace default tag with 2 tags: media and the specific to the type of media. I had a bug when filtering the results when there was a null value, so I added an extra filter.

src/api/apis/OMDbAPI.ts
@@ -67,11 +67,11 @@ export class OMDbAPI extends APIModel {
 		this.apiName = 'OMDbAPI';
 		this.apiDescription = 'A free API for Movies, Series and Games.';
 		this.apiUrl = 'https://www.omdbapi.com/';
-		this.types = [MediaType.Movie, MediaType.Series, MediaType.Game];
+		this.types = [MediaType.Movie, MediaType.Series, MediaType.Videogame];
 		this.typeMappings = new Map<string, string>();
 		this.typeMappings.set('movie', 'movie');
 		this.typeMappings.set('series', 'series');
-		this.typeMappings.set('game', 'game');
+		this.typeMappings.set('videogame', 'videogame');
 	}
 
 	async searchByTitle(title: string): Promise<MediaTypeModel[]> {
@@ -141,7 +141,7 @@ export class OMDbAPI extends APIModel {
 						id: result.imdbID,
 					}),
 				);
-			} else if (type === 'game') {
+			} else if (type === 'videogame') {
 				ret.push(
 					new GameModel({
 						type: type,
@@ -254,7 +254,7 @@ export class OMDbAPI extends APIModel {
 					personalRating: 0,
 				},
 			});
-		} else if (type === 'game') {
+		} else if (type === 'videogame') {
 			return new GameModel({
 				type: type,
 				title: result.Title,

src/main.ts
@@ -206,7 +206,7 @@ export default class MediaDbPlugin extends Plugin {
 		}
 
 		// filter the results
-		apiSearchResults = apiSearchResults.filter(x => types.contains(x.type));
+		apiSearchResults = apiSearchResults.filter(Boolean).filter(x => types.contains(x.type));
 
 		let selectResults: MediaTypeModel[];
 		const proceed: boolean = false;

src/api/apis/GiantBombAPI.ts
@@ -18,7 +18,7 @@ export class GiantBombAPI extends APIModel {
 		this.apiName = 'GiantBombAPI';
 		this.apiDescription = 'A free API for games.';
 		this.apiUrl = 'https://www.giantbomb.com/api';
-		this.types = [MediaType.Game];
+		this.types = [MediaType.Videogame];
 	}
 
 	async searchByTitle(title: string): Promise<MediaTypeModel[]> {
@@ -136,7 +136,7 @@ export class GiantBombAPI extends APIModel {
 			| undefined;
 
 		return new GameModel({
-			type: MediaType.Game,
+			type: MediaType.Videogame,
 			title: result.name,
 			englishTitle: result.name,
 			year: year,

src/api/apis/MobyGamesAPI.ts
@@ -22,7 +22,7 @@ export class MobyGamesAPI extends APIModel {
 		this.apiName = 'MobyGamesAPI';
 		this.apiDescription = 'A free API for games.';
 		this.apiUrl = 'https://api.mobygames.com/v1';
-		this.types = [MediaType.Game];
+		this.types = [MediaType.Videogame];
 	}
 
 	async searchByTitle(title: string): Promise<MediaTypeModel[]> {
@@ -55,7 +55,7 @@ export class MobyGamesAPI extends APIModel {
 		for (const result of data.games) {
 			ret.push(
 				new GameModel({
-					type: MediaType.Game,
+					type: MediaType.Videogame,
 					title: result.title,
 					englishTitle: result.title,
 					year: new Date(result.platforms[0].first_release_date).getFullYear().toString(),
@@ -90,7 +90,7 @@ export class MobyGamesAPI extends APIModel {
 		const result = data.games[0];
 
 		return new GameModel({
-			type: MediaType.Game,
+			type: MediaType.Videogame,
 			title: result.title,
 			englishTitle: result.title,
 			year: new Date(result.platforms[0].first_release_date).getFullYear().toString(),

src/api/apis/SteamAPI.ts
@@ -138,9 +138,9 @@ export class SteamAPI extends APIModel {
 		this.apiName = 'SteamAPI';
 		this.apiDescription = 'A free API for all Steam games.';
 		this.apiUrl = 'https://www.steampowered.com/';
-		this.types = [MediaType.Game];
+		this.types = [MediaType.Videogame];
 		this.typeMappings = new Map<string, string>();
-		this.typeMappings.set('game', 'game');
+		this.typeMappings.set('videogame', 'videogame');
 	}
 
 	async searchByTitle(title: string): Promise<MediaTypeModel[]> {
@@ -164,7 +164,7 @@ export class SteamAPI extends APIModel {
 		for (const result of data) {
 			ret.push(
 				new GameModel({
-					type: MediaType.Game,
+					type: MediaType.Videogame,
 					title: result.name,
 					englishTitle: result.name,
 					year: '',
@@ -216,7 +216,7 @@ export class SteamAPI extends APIModel {
 		}
 
 		return new GameModel({
-			type: MediaType.Game,
+			type: MediaType.Videogame,
 			title: result.name,
 			englishTitle: result.name,
 			year: new Date(result.release_date.date).getFullYear().toString(),

src/models/BoardGameModel.ts
@@ -51,7 +51,7 @@ export class BoardGameModel extends MediaTypeModel {
 	}
 
 	getTags(): string[] {
-		return [mediaDbTag, 'boardgame'];
+		return ['media', 'boardgames'];
 	}
 
 	getMediaType(): MediaType {

src/models/BookModel.ts
@@ -51,7 +51,7 @@ export class BookModel extends MediaTypeModel {
 	}
 
 	getTags(): string[] {
-		return [mediaDbTag, 'book'];
+		return ['media', 'books'];
 	}
 
 	getMediaType(): MediaType {

src/models/ComicMangaModel.ts
@@ -61,7 +61,7 @@ export class ComicMangaModel extends MediaTypeModel {
 	}
 
 	getTags(): string[] {
-		return [mediaDbTag, 'manga', 'light-novel', 'comicbook'];
+		return ['media', 'manga', 'light-novel', 'comicbook'];
 	}
 
 	getMediaType(): MediaType {

commit 74ffdec087dc4c4ded8a962bff61971538a34175

Fix a bug that I introduced in 62347f6 regarding searching for videogames in omdb

src/api/apis/OMDbAPI.ts
@@ -71,7 +71,7 @@ export class OMDbAPI extends APIModel {
 		this.typeMappings = new Map<string, string>();
 		this.typeMappings.set('movie', 'movie');
 		this.typeMappings.set('series', 'series');
-		this.typeMappings.set('videogame', 'videogame');
+		this.typeMappings.set('game', 'videogame');
 	}
 
 	async searchByTitle(title: string): Promise<MediaTypeModel[]> {
@@ -194,6 +194,8 @@ export class OMDbAPI extends APIModel {
 		if (type === undefined) {
 			throw Error(`${result.Type.toLowerCase()} is an unsupported type.`);
 		}
+
+		// console.debug(result);
 
 		if (type === 'movie') {
 			return new MovieModel({

src/main.ts
@@ -206,7 +206,7 @@ export default class MediaDbPlugin extends Plugin {
 		}
 
 		// filter the results
-		apiSearchResults = apiSearchResults.filter(Boolean).filter(x => types.contains(x.type));
+		apiSearchResults = apiSearchResults.filter(x => types.contains(x.type));
 
 		let selectResults: MediaTypeModel[];
 		const proceed: boolean = false;